a3ff68d3c2ef6c6e029556b3217191f9a96014ca
codemakeworldgreat <huioh89@gmail.com>
2026-01-06 06:26:27 +0000

    update

--- lang/deal/flush.go
+++ lang/deal/flush.go
@@ -9,21 +9,29 @@ func DealFlush(color[] int,list[]int)[]int{
    for i,v:=range count{
 	if v>=5{
 		index:=i
-       result= FlushCards(index,list)
-	   fmt.Println(result)
+       result= findCards(index,list)
+	    sort:=sortNumber (result)
+		straights:=dealstraight(sort)
+		if len(straights)>0{
+			if straights[0]==14&&straights[4]==10{
+              //最大同花顺
+			}else{
+             //同花顺
+			}
+		}
+	   
 	}else{
 	   continue
 	}
    }
    return result
 }
-func FlushCards(index int,list[] int)[]int{
+func findCards(index int,list[] int)[]int{
 	 flush:=make([]int,0,len(list))
      for _,v:=range list{
 		if v&3==index{
          flush=append(flush,v>>2)
 		}
 	 }
-	 sortflush:=sortNumber(flush)
-	 return sortflush
+	 return flush
 }
--- lang/deal/level.go
+++ lang/deal/level.go
@@ -1,6 +1,8 @@
 package deal
 
-func  (c*Counts)isFour(number[]int){
+
+
+func  (c*Counts)isFour(number[]int)[]int{
 	max:=0
 	
     if c.Four!=0{
@@ -18,6 +20,37 @@ func  (c*Counts)isFour(number[]int){
 		 }
 	   }
 	  fourkind:=make([]int,0)
-	  fourkind=append(fourkind,c.Four,max)
+	  fourkind=append(fourkind,8,c.Four,max)
+	  return fourkind
 	}
+	return nil
+}
+func (c*Counts)isThree()[]int{
+     data:=make([]int,0,3)
+   if c.Three!=0{
+	if len(c.TwoPairs)>0{
+      data=append(data,7,c.Three,c.TwoPairs[0])
+	  return data
+	}else{
+		data=append(data,4,c.Three,c.HighCards[0])
+		return data
+	}
+   }
+   return nil
+}
+func (c*Counts)isTwo()[]int{
+	data:=make([]int,0)
+	if len(c.TwoPairs)>1{
+	 data=append(data,3,c.TwoPairs[0],c.TwoPairs[1],c.HighCards[0])
+	 return data
+	}else if len(c.TwoPairs)==1{
+		data=append(data,2,c.TwoPairs[0],c.HighCards[0])
+		return data
+	}
+	return nil
+}
+func (c*Counts)isHigh()[]int{
+	result:=[]int{1,}
+	result=append(result,c.HighCards...)
+   return result
 }
